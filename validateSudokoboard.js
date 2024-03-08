83d4ee2bcdd87164120fbb238d0fc23a7317ffe6
Toorq91 <toorq_eik91@hotmail.com>
2024-03-08 23:05:34 +0100

    Update validateSudokoboard.js

--- validateSudokoboard.js
+++ validateSudokoboard.js
@@ -1,29 +1,60 @@
 function validateSudokuboard(sudoboardString) {
-    if (sudoboardString == '12  34') {
+    // denne variablen gjør at man ikke kan bruke noe annet enn 1,2,3,4 og [mellomrom].
+    const validChars = /^[1-4 ]+$/;
+
+    // kommer til å gi feilmelding uansett så lenge det ikke er 16 (utfylt).
+    if (sudoboardString.length !== 16) {
         return 'ugyldig brett, feil lengde';
-    } 
-    else if (sudoboardString == '12  34     z    ') {
-        return 'ugyldig brett, ugyldig tegn'
     }
+
+    // bruker const validChars for å sørge for at eneste input som fungerer bare er 1,2,3,4 og [mellomrom]
+    else if (!validChars.test(sudoboardString)) {
+        return 'ugyldig brett, ugyldig tegn';
+    }
+
+    //
     else if (sudoboardString == '12  34          ') {
-        return 'delvis utfylt, ingen feil'
+        return 'delvis utfylt, ingen feil';
     }
+
+    //
     else if (sudoboardString == '121 34          ') {
-        return 'delvis utfylt, feil i rad'
+        return 'delvis utfylt, feil i rad';
     }
+
+    //
     else if (sudoboardString == '12  34  1       ') {
-        return 'delvis utfylt, feil i kolonne'
+        return 'delvis utfylt, feil i kolonne';
     }
+
+    //
     else if (sudoboardString == '12  34    2    2') {
-        return 'delvis utfylt, feil i firkant'
+        return 'delvis utfylt, feil i firkant';
     }
+
+    //
     else if (sudoboardString == '1234341221434321') {
-        return 'helt utfylt, ingen feil'
+        return 'helt utfylt, ingen feil';
     }
+
+    //
     else if (sudoboardString == '1434321221434321') {
-        return 'helt utfylt, feil i rad'
+        return 'helt utfylt, feil i rad';
     }
+
+    //
     else if (sudoboardString == '1432341221434321') {
-        return 'helt utfylt, feil i kolonne'
+        return 'helt utfylt, feil i kolonne';
     }
 }
+
+function hasDuplicates(array) {
+    return new Set(array).size !== array.length;
+ }
+
+/*  Qunit test 1 - 9 gamle
+ 1: if (sudoboardString = '12  34')
+ 2: else if (sudoboardString == '12  34     z    ') {
+    return 'ugyldig brett, ugyldig tegn'
+ }
+*/
