c062e2cf4b9ec02b7629dcc7985c1e87e93a36a7
Toorq91 <toorq_eik91@hotmail.com>
2024-03-10 20:02:53 +0100

    fjernet all unødvending read only tekst

--- validateSudokoboard.js
+++ validateSudokoboard.js
@@ -1,10 +1,9 @@
 function validateSudokuboard(sudoboardString) {
     // denne variablen gjør at man ikke kan bruke noe annet enn 1,2,3,4 og [mellomrom].
     const validChars = /^[1-4 ]+$/;
+    
+    //
     const board = stringToArrayWithSpaces(sudoboardString)
-    // Remove spaces from the input and convert it to a 2D array
-    // const board = sudoboardString.replace(/ /g, '').split('').map(Number);
-
 
     // kommer til å gi feilmelding uansett så lenge det ikke er 16 (utfylt).
     if (sudoboardString.length !== 16) {
@@ -47,61 +46,6 @@ function validateSudokuboard(sudoboardString) {
     else if (fullyFilled(board)) {
         return 'helt utfylt, ingen feil';
     }
-
-
-    // else if (partiallyFilled(board)) {
-    //     if (hasDuplicates(board)) {
-    //         return 'delvis utfylt, feil i rad';
-    //     } else if (hasDuplicates(getColumns(board))) {
-    //         return 'delvis utfylt, feil i kolonne';
-    //     } else if (hasDuplicates(getSquares(board))) {
-    //         return 'delvis utfylt, feil i firkant';
-    //     } else {return 'delvis utfylt, ingen feil';}
-    // }
-
-    // else if (fullyFilled(board)) {
-    //     if (hasDuplicates(board)) {
-    //         return 'helt utfylt, feil i rad';
-    //     } else if (hasDuplicates(getColumns(board))) {
-    //         return 'helt utfylt, feil i kolonne';
-    //     } else if (hasDuplicates(getSquares(board))) {
-    //         return 'helt utfylt, feil i firkant';
-    //     } else {return 'helt utfylt, ingen feil';}
-    // }
-    
-    // else if (sudoboardString == '12  34          ') {
-    //     return 'delvis utfylt, ingen feil';
-    // }
-
-    //
-    // else if (sudoboardString == '121 34          ') {
-    //     return 'delvis utfylt, feil i rad';
-    // }
-
-    // //
-    // else if (sudoboardString == '12  34  1       ') {
-    //     return 'delvis utfylt, feil i kolonne';
-    // }
-
-    // //
-    // else if (sudoboardString == '12  34    2    2') {
-    //     return 'delvis utfylt, feil i firkant';
-    // }
-
-    //
-    // else if (sudoboardString == '1234341221434321') {
-    //     return 'helt utfylt, ingen feil';
-    // }
-
-    // //
-    // else if (sudoboardString == '1434321221434321') {
-    //     return 'helt utfylt, feil i rad';
-    // }
-
-    // //
-    // else if (sudoboardString == '1432341221434321') {
-    //     return 'helt utfylt, feil i kolonne';
-    // }
 }
 
 function stringToArrayWithSpaces(inputString) {
@@ -115,22 +59,6 @@ function stringToArrayWithSpaces(inputString) {
     return numericArray;
 }
 
-// function stringToCharArray(sudoboardString) {
-//     return sudoboardString.replace(/ /g, '').split('').map(Number);
-// }
-
-// function stringToCharArray(inputString) {
-//     // Use split to get an array of characters
-//     var resultArray = inputString.split('');
-  
-//     // Use map to convert each character to a number if it's a digit
-//     resultArray = resultArray.map(function (char) {
-//       return isNaN(char) ? char : Number(char);
-//     });
-  
-//     // Return the resulting array
-//     return resultArray;
-//   }
 
 /* tar inn en liste (board) som representerer et sudokubrett 
 (board) er skrevet som en lang liste: 1d (bare èn dimenson)
@@ -219,49 +147,6 @@ function hasDuplicatesInRows(board) {
     return false;
 }
 
-// function hasDuplicates(board) {
-//     // Helper function to check for duplicates in an array
-//     function hasDuplicatesInArray(array) {
-//         const seen = {};
-//         for (const num of array) {
-//             if (num !== ' ' && seen[num]) {
-//                 return true;
-//             }
-//             seen[num] = true;
-//         }
-//         return false;
-//     }
-
-//     // Check for duplicates in each row
-//     for (let i = 0; i < board.length; i += 4) {
-//         const row = board.slice(i, i + 4).filter(num => num !== ' ');
-//         if (hasDuplicatesInArray(row)) {
-//             return true;
-//         }
-//     }
-
-//     return false;
-// }
-
-// // Helper function to check for duplicate numbers in an array
-// function hasDuplicates(arr) {
-//     // variabel for et tomt objekt
-//     const seen = {};
-
-//     // går igjennom hvert element i lista (posisjon: 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
-//     for (const num of arr) {
-//         // Sjekker om det er et tall og sjekker om dette tallet har blitt sett allerede.
-//         if (num !== ' ' && seen[num]) {
-//             // Hvis begge betingelsene i linjen over er sant så returnerer den sant = "true" (da er det 2 av samme tall)
-//             return true;
-//         }
-//         // Returnerer sant = "true" dersom tallet er sett
-//         seen[num] = true;
-//     }
-//     // Hvis ikke det som står over, er det ikke duplikat av tallet og kan bli brukt.
-//     return false;
-// }
-
 function partiallyFilled(board) {
     const numberOfNumbers = board.filter(element => typeof element === 'number' && !isNaN(element)).length;
     return numberOfNumbers !== 16;
@@ -271,49 +156,3 @@ function fullyFilled(board) {
     const numberOfNumbers = board.filter(element => typeof element === 'number' && !isNaN(element)).length;
     return numberOfNumbers === 16;
 }
-
-
-// function getRows(board) {
-//     //
-//     const rows = [];
-
-//     for (let row = 0; row < 4; row++) {
-//         rows.push([])
-
-//         for (let col = 0; col < 4; col++) {
-//             rows[row].push(board[row * 4 + col]);
-//         }
-//     }
-//     return rows.flat();
-// }
-
-
-/*
-
-function validateSudokuboard(sudokuboard) {
-    // Remove spaces from the input and convert it to a 2D array
-    const board = sudokuboard.replace(/ /g, '').split('').map(Number);
-    
-    // Check for duplicate numbers in rows, columns, and 3x3 squares
-    if (hasDuplicates(board) || hasDuplicates(getColumns(board)) || hasDuplicates(getSquares(board))) {
-        return 'feil i sudoku';
-    }
-    
-    return 'delvis utfylt, ingen feil';
-}
-
-
-*/
-
-
-
-// function hasDuplicates(array) {
-//     return new Set(array).size !== array.length;
-// }
-
-/*  Qunit test 1 - 9 gamle
- 1: if (sudoboardString = '12  34')
- 2: else if (sudoboardString == '12  34     z    ') {
-    return 'ugyldig brett, ugyldig tegn'
- }
-*/
